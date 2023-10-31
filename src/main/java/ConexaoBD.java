6efd9c5660ea4a57ba18a9bde410bd92a116c94a
Andreia Machion <amachion@gmail.com>
2023-10-31 19:38:52 -0300

    Código aula 30/10

---
+++ src/main/java/ConexaoBD.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+public class ConexaoBD {
+    //colocar os valores do seu aiven
+    private static String usuario = "avnadmin";
+    private static String senha = "sua senha aqui";
+    private static String host = "mysql-3d189786-saojudas-f46b.aivencloud.com";
+    private static String porta = "21845";
+    private static String bd = "defaultdb";
+    public static Connection obtemConexao (){
+        try {
+            Connection c = DriverManager.getConnection(
+            "jdbc:mysql://" + host + ":" + porta + "/" + bd + "?user=" + usuario + 
+            "&password=" + senha + 
+            "&useTimezone=true&serverTimezone=America/Sao_Paulo");
+            return c;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    //public static void main(String[] args) throws Exception{
+       // System.out.println(obtemConexao());
+    //}
+}
